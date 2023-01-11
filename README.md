90906b8e3c08f86ef706fb532143cc58c41e5d41
Hugh Crawford <hcrawford23@yahoo.com>
2023-01-11 17:06:48 +0000

    Create random message generator

---
+++ README.md
@@ -0,0 +1,2 @@
+#Star Wars Name Generator
+
--- mixed-messages.js
+++ mixed-messages.js
@@ -1,1 +1,17 @@
-console.log('I will eventually display a random message!');
+const firstName = ['Luke', 'Leia', 'Han', 'R2', 'C3'];
+const lastName = ['Skywalker', 'Organa', 'Solo', 'D2', 'PO'];
+const homePlanet = ['Tatooine', 'Alderaan', 'Corellia', 'Naboo', 'Affa'];
+
+let randomFirstName = firstName[(Math.random() * firstName.length) | 0];
+let randomSecondName = lastName[(Math.random() * lastName.length) | 0];
+let randomHomePlanet = homePlanet[(Math.random() * homePlanet.length) | 0];
+
+let randomMessage = () => {
+    console.log('The name on your galactic passport is:');
+    console.log(randomFirstName + ' ' + randomSecondName);
+
+    console.log('You were born on the planet:');
+    console.log(randomHomePlanet);
+};
+
+randomMessage();
