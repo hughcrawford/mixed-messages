a2d355bd2d3a4c3cb9afb77a1316440c5be06115
Hugh Crawford <hcrawford23@yahoo.com>
2023-01-11 19:26:19 +0000

    Add UI

--- mixed-messages.js
+++ mixed-messages.js
@@ -2,16 +2,20 @@ const firstName = ['Luke', 'Leia', 'Han', 'R2', 'C3'];
 const lastName = ['Skywalker', 'Organa', 'Solo', 'D2', 'PO'];
 const homePlanet = ['Tatooine', 'Alderaan', 'Corellia', 'Naboo', 'Affa'];
 
-let randomFirstName = firstName[(Math.random() * firstName.length) | 0];
-let randomSecondName = lastName[(Math.random() * lastName.length) | 0];
-let randomHomePlanet = homePlanet[(Math.random() * homePlanet.length) | 0];
-
 let randomMessage = () => {
+
+    const randomFirstName = firstName[(Math.random() * firstName.length) | 0];
+    const randomSecondName = lastName[(Math.random() * lastName.length) | 0];
+    const randomHomePlanet = homePlanet[(Math.random() * homePlanet.length) | 0];
+
+    /*
     console.log('The name on your galactic passport is:');
     console.log(randomFirstName + ' ' + randomSecondName);
 
     console.log('You were born on the planet:');
     console.log(randomHomePlanet);
-};
+    */
 
-randomMessage();
+    document.getElementById("nameOutput").innerHTML = 'Your name is: ' + randomFirstName + ' ' + randomSecondName;
+    document.getElementById("planetOutput").innerHTML = 'Your home planet is: ' + randomHomePlanet;
+};
